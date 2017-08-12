1a1b5eb976ba55eec21857b9d4c5e26dab041595
Orlando Wilson <landough@Orlandos-iMac.fios-router.home>
2017-08-11 22:41:18 -0400

    route updates

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ import { index } from './controllers/index';
 const app = express();
 const PORT = 3000;
 
-app.get('/', index);
+app.route('/').get(index);
 app.listen(PORT, () => console.log(`Express Server Running on port ${PORT}`));
 
 module.exports = app;
